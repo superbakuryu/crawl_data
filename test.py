e69f55291e954bd662a01587e39424084462d3c6
superbakuryu <superbakuryu@gmail.com>
2021-09-30 00:30:39 +0700

    140

--- test.py
+++ test.py
@@ -1,14 +1,32 @@
 import requests
 import xlsxwriter
 
+# from random_user_agent.user_agent import UserAgent
+# from random_user_agent.params import SoftwareName, OperatingSystem
+# software_names = [SoftwareName.ANDROID.value]
+# operating_systems = [OperatingSystem.WINDOWS.value, OperatingSystem.LINUX.value, OperatingSystem.MAC.value]   
+
+# user_agent_rotator = UserAgent(software_names=software_names, operating_systems=operating_systems, limit=1000)
+
+# # Get list of user agents.
+# user_agents = user_agent_rotator.get_user_agents()
+
+# user_agent_random = user_agent_rotator.get_random_user_agent()
+# print(user_agent_random)
+
+def recaptcha():
+    url = 'https://www.wongnai.com/_gateway/recaptcha'
+    response = requests.post(url)
+    print(response.text)
+
 def get_page(num):
     try:
         url = f'https://www.wongnai.com/_api/users/nickythedevil/photos.json?_v=6.042&locale=th&page.number={num}&page.size=1'
+        # headers = {'User-Agent': user_agent_random}
         headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
-
         response = requests.get(url, headers=headers)
         j_response = response.json()
-        return j_response
+        # return j_response
         link = j_response.get('page').get('entities')[0].get('largeUrl')
         return link
     except:
@@ -16,13 +34,13 @@ def get_page(num):
 
 def export_excel(num_continue):
     # Create a workbook and add a worksheet.
-    workbook = xlsxwriter.Workbook('image_url_{num_continue}.xlsx')
+    workbook = xlsxwriter.Workbook(f'image_url_{num_continue}.xlsx')
     worksheet = workbook.add_worksheet()
 
     # Some data we want to write to the worksheet.
     list_cus = [
     ]
-    for i in range(1, 20000):
+    for i in range(num_continue, 20000):
         print(i)
         link = get_page(i)
         lst = [link]
@@ -39,7 +57,9 @@ def export_excel(num_continue):
     print("Done")
     return True
 
-num_continue = 101
-print(get_page(101))
+num_continue = 140
+print(get_page(num_continue))
 
-# export_excel(101)
+# export_excel(num_continue)
+
+# recaptcha()
